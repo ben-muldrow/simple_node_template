3925ed368f137d308ad8ffca373f69a22430f372
Benjamin Muldrow <benjaminmuldrow@Benjamins-MacBook.local>
2017-04-30 18:34:16 -0400

    added some stuff

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app     = express();
-var router = express.Router();
+var router  = express.Router();
 
 router.get('/', (req, res) => {
-    res.json({ message : 'hello, shodan'});
+    //res.json({ message : 'hello, shodan'});
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.use('/api',router);
-app.listen(8080);
+app.use(router);
+app.listen(3000);
